b889dcb214b9e253d166bf73020385361a786541
Anh Quach <tuananh.quach@moca.vn>
2022-07-10 00:23:20 +0700

    Fix count

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_efs_file_system" "efs" {
 resource "aws_efs_mount_target" "efs_mt" {
   count = var.create_efs ? length(var.subnets) : 0
 
-  file_system_id  = aws_efs_file_system.efs.id
+  file_system_id  = aws_efs_file_system.efs[0].id
   subnet_id       = var.subnets[count.index]
   security_groups = var.security_groups
 }
